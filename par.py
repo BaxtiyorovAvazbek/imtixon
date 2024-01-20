dd19a7fbacd976f04b1731f57f1ca019fe3661a4
Avazbek00 <bravlkvas748@gmail.com>
2024-01-20 14:02:31 +0300

    finished 14:02

---
+++ par.py
@@ -0,0 +1,60 @@
+# 1 masala javobi
+
+numbers = (1,2,3,4,6,7,8,9,10)
+
+x = 5
+
+if x in numbers:
+    print("Bor")
+
+else:
+    print(5)
+
+number = (1,2,5,3,4,8,9,6,10)
+
+y = 7
+
+if y in number:
+    print("Bor")
+
+else:
+    print(7)
+
+num = (2,3,5,4,10,8,9,6,7)
+
+n = 1
+
+if n in num:
+    print("Bor")
+
+else:
+    print(1)
+
+
+
+# 2 masala javobi
+
+car = ("3/4/5/6/7/8/11/0")
+
+A = int(input("Aholi soni: "))
+
+if car in "A":
+    print("Moshina: ", A // 3)
+
+else:
+    print("Moshina: ", A // 4)
+
+
+
+# 3 masala javobi
+
+son = (["2, 4, 6, 11"])
+
+
+foiz = int(input("Foiz: "))
+
+if "son" == "foiz":
+    print([1,2,3,5.5])
+
+else:
+    print([1,2,3,5.5])
